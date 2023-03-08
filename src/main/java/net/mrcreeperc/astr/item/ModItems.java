a11c9e228cb79b3a2468b62b8adae31d63aeee26
MrCreeperH <olgaakshidi.ru@gmail.com>
2023-03-08 14:22:03 +0300

    add new Item Ghost spawn egg

--- src/main/java/net/mrcreeperc/astr/item/ModItems.java
+++ src/main/java/net/mrcreeperc/astr/item/ModItems.java
@@ -1,17 +1,24 @@
 package net.mrcreeperc.astr.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.mrcreeperc.astr.AstrMod;
+import net.mrcreeperc.astr.entity.ModEntities;
 
 public class ModItems {
 
+
+    //Яйца призыва
+    public static final Item GHOST_SPAWN_EGG = registerItem("ghost_spawn_egg",
+            new SpawnEggItem(ModEntities.ASTRAL_GHOST, 0x5065a6, 0x7190f0,
+                    new FabricItemSettings().group(ModItemGroup.ASTRAL_TRAVEL)));
+
+
+    //Предметы
     public static final Item ASTRAL_STONE = registerItem("astral_stone",
-            new Item(new FabricItemSettings().maxCount(12).group(ModItemGroup.ASTRAL_TRAVEL)));
+            new Item(new FabricItemSettings().maxCount(12).group(ModItemGroup.ASTRAL_TRAVEL).food(FoodComponents.COOKIE)));
 
 
     private static Item registerItem(String name, Item item) {
